36407cda8c6998d8f67e2c36f9e10276eb592949
josamcode <gergessamuel100@gmail.com>
2026-05-02 05:30:15 +0300

    feat(users): add unspecified education stages

--- src/constants/education.js
+++ src/constants/education.js
@@ -1,15 +1,19 @@
 const EDUCATION_STAGE_DEFINITIONS = Object.freeze([
+  { value: 'kindergarten_unspecified_year', group: 'kindergarten', context: 'kindergarten' },
   { value: 'kindergarten_kg1', group: 'kindergarten', context: 'kindergarten' },
   { value: 'kindergarten_kg2', group: 'kindergarten', context: 'kindergarten' },
+  { value: 'primary_unspecified_year', group: 'primary', context: 'school' },
   { value: 'primary_grade_1', group: 'primary', context: 'school' },
   { value: 'primary_grade_2', group: 'primary', context: 'school' },
   { value: 'primary_grade_3', group: 'primary', context: 'school' },
   { value: 'primary_grade_4', group: 'primary', context: 'school' },
   { value: 'primary_grade_5', group: 'primary', context: 'school' },
   { value: 'primary_grade_6', group: 'primary', context: 'school' },
+  { value: 'preparatory_unspecified_year', group: 'preparatory', context: 'school' },
   { value: 'preparatory_grade_1', group: 'preparatory', context: 'school' },
   { value: 'preparatory_grade_2', group: 'preparatory', context: 'school' },
   { value: 'preparatory_grade_3', group: 'preparatory', context: 'school' },
+  { value: 'secondary_unspecified_year', group: 'secondary_general', context: 'school' },
   { value: 'secondary_general_year_1', group: 'secondary_general', context: 'school' },
   { value: 'secondary_general_year_2', group: 'secondary_general', context: 'school' },
   { value: 'secondary_general_year_3', group: 'secondary_general', context: 'school' },
@@ -26,12 +30,16 @@ const EDUCATION_STAGE_DEFINITIONS = Object.freeze([
   { value: 'secondary_technical_technical_arts_year_2', group: 'secondary_technical', context: 'school' },
   { value: 'secondary_technical_technical_arts_year_3', group: 'secondary_technical', context: 'school' },
   { value: 'finished_education', group: 'finished', context: 'finished' },
+  { value: 'university_unspecified_year', group: 'university', context: 'university' },
   { value: 'university_year_1', group: 'university', context: 'university' },
   { value: 'university_year_2', group: 'university', context: 'university' },
   { value: 'university_year_3', group: 'university', context: 'university' },
   { value: 'university_year_4', group: 'university', context: 'university' },
   { value: 'university_year_5', group: 'university', context: 'university' },
   { value: 'university_graduate', group: 'university', context: 'university' },
+  { value: 'uneducated', group: 'other', context: 'other' },
+  { value: 'literacy', group: 'other', context: 'other' },
+  { value: 'student', group: 'other', context: 'other' },
 ]);
 
 const EDUCATION_STAGE_VALUES = EDUCATION_STAGE_DEFINITIONS.map((definition) => definition.value);
@@ -43,6 +51,7 @@ const EDUCATION_STAGE_GROUP_VALUES = Object.freeze([
   'secondary_technical',
   'finished',
   'university',
+  'other',
 ]);
 const EDUCATION_STAGE_FILTER_VALUES = Object.freeze([
   ...new Set([...EDUCATION_STAGE_VALUES, ...EDUCATION_STAGE_GROUP_VALUES]),
